aaac4d60bd4ec3965d57eb5ea7d8c3d0b317bb78
marconotaro <notaromarco90@gmail.com>
2020-05-18 15:39:18 +0200

    add obogaf::parser exporter

--- script/GOscript.pl
+++ script/GOscript.pl
@@ -3,7 +3,7 @@
 ## loading obogaf::parser and 
 use strict;
 use warnings;
-use obogaf::parser;
+use obogaf::parser qw(:all);
 
 ## elapased time 
 use Time::HiRes qw(time);
@@ -14,7 +14,7 @@ use File::Path qw(make_path);
 
 ## create folder where storing example I/O files
 use File::Path qw(make_path);
-my $basedir= "example/data/";
+my $basedir= "data/";
 make_path($basedir) unless(-d $basedir);
 
 ## note: if you want to store data in your home, use File::HomeDir
@@ -31,8 +31,21 @@ my $obofile= $basedir."gobasic.obo";
 my $gobo= qx{wget --output-document=$obofile http://purl.obolibrary.org/obo/go/go-basic.obo};
 print "GO obo file downloaded: done\n\n";
 
+## shrink GO obo file to a subset of terms
+my @terms = qw(GO:0000002 GO:0000003 GO:0000018 GO:0000030 GO:0000038);
+my $termsfile= $basedir."goterms.txt";
+open OUT, "> $termsfile";
+foreach my $go (@terms){print OUT "$go\n";}
+close OUT;
+
+$res= obo_filter($obofile, $termsfile);
+my $newobo= $basedir."go-shrunk.obo"; 
+open OUT, ">", $newobo; 
+print OUT "${$res}";
+close OUT;
+
 ## extract edges from GO obo file
-my $gores= obogaf::parser::build_edges($obofile);
+my $gores= build_edges($obofile);
 my $goedges= $basedir."gobasic-edges.txt"; ## go edges file declared here 
 open FH, "> $goedges"; 
 print FH "${$gores}"; ## scalar dereferencing
@@ -46,7 +59,7 @@ my %aspects= (biological_process => "BP", molecular_function => "MF", cellular_c
 foreach my $domain (@domains){
     my $outfile= $basedir."gobasic-edges"."$aspects{$domain}".".txt";
     open FH, "> $outfile";
-    my $domainres= obogaf::parser::build_subonto($goedges, $domain);
+    my $domainres= build_subonto($goedges, $domain);
     print FH "${$domainres}";
     close FH;
 }
@@ -54,26 +67,26 @@ print "build edges for each GO subontology: done\n\n";
 
 ## make stats on the whole GOobo file
 ($parentIndex, $childIndex)= (1,2);
-$res= obogaf::parser::make_stat($goedges, $parentIndex, $childIndex);
+$res= make_stat($goedges, $parentIndex, $childIndex);
 print "$res";
 print "\nGO stats: done\n\n";
 
 ## make stats on a GO-BP subontology
 my $goedgesbp= $basedir."gobasic-edgesBP.txt";
 ($parentIndex, $childIndex)= (0,1);
-$res= obogaf::parser::make_stat($goedgesbp, $parentIndex, $childIndex);
+$res= make_stat($goedgesbp, $parentIndex, $childIndex);
 print "$res";
 print "\nGO BP stats: done\n\n";
 
 ## compute parents and children list (whole ontology)
 $parlist= $basedir."gobasic-parGO.txt";
-$pares= obogaf::parser::get_parents_or_children_list($goedges, 1,2, "parents");
+$pares= get_parents_or_children_list($goedges, 1,2, "parents");
 open FH, "> $parlist";
 foreach my $k (sort{$a cmp $b} keys %$pares) { print FH "$k $$pares{$k}\n";} ## parents  list
 close FH;
 
 $chdlist= $basedir."gobasic-chdGO.txt";
-$chdres= obogaf::parser::get_parents_or_children_list($goedges, 1,2, "children");
+$chdres= get_parents_or_children_list($goedges, 1,2, "children");
 open FH, "> $chdlist";
 foreach my $k (sort{$a cmp $b} keys %$chdres) { print FH "$k $$chdres{$k}\n";} ## children list
 close FH;
@@ -82,13 +95,13 @@ print "\nGO parents/children list: done\n\n";
 
 ## compute parents and children list (GO-BP subontology)
 $parlist= $basedir."gobasic-parGO-BP.txt";
-$pares= obogaf::parser::get_parents_or_children_list($goedgesbp, 0,1, "parents");
+$pares= get_parents_or_children_list($goedgesbp, 0,1, "parents");
 open FH, "> $parlist";
 foreach my $k (sort{$a cmp $b} keys %$pares) { print FH "$k $$pares{$k}\n";} ## parents  list
 close FH;
 
 $chdlist= $basedir."gobasic-chdGO-BP.txt";
-$chdres= obogaf::parser::get_parents_or_children_list($goedgesbp, 0,1, "children");
+$chdres= get_parents_or_children_list($goedgesbp, 0,1, "children");
 open FH, "> $chdlist";
 foreach my $k (sort{$a cmp $b} keys %$chdres) { print FH "$k $$chdres{$k}\n";} ## children list
 close FH;
@@ -102,7 +115,7 @@ my $goachicken= qx{wget --output-document=$goafile ftp://ftp.ebi.ac.uk/pub/datab
 
 ## extract GO annotation from GOA database (CHICKEN organism)
 ($geneindex, $classindex)= (1, 4);
-($res, $stat)= obogaf::parser::gene2biofun($goafile, $geneindex, $classindex);
+($res, $stat)= gene2biofun($goafile, $geneindex, $classindex);
 my $goaout= $basedir."chicken.uniprot2go.txt";
 open FH, "> $goaout";
 foreach my $k (sort{$a cmp $b} keys %$res) { print FH "$k $$res{$k}\n";} ## dereferencing
@@ -116,7 +129,7 @@ my $goafileOld= $basedir."goa_chicken.gaf.128.gz"; ## goa annotation file declar
 my $goachickenOld= qx{wget --output-document=$goafileOld ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/old/CHICKEN/goa_chicken.gaf.128.gz};
 
 ## map GO terms between release
-($res, $stat)= obogaf::parser::map_OBOterm_between_release($obofile, $goafileOld, $classindex);
+($res, $stat)= map_OBOterm_between_release($obofile, $goafileOld, $classindex);
 my $mapfile= $basedir."chicken.goa.mapped.txt";
 open FH, "> $mapfile"; 
 print FH "${$res}";
--- script/HPOscript.pl
+++ script/HPOscript.pl
@@ -3,7 +3,7 @@
 ## loading obogaf::parser and useful Perl module
 use strict;
 use warnings;
-use obogaf::parser; 
+use obogaf::parser qw(:all); 
 
 ## elapased time 
 use Time::HiRes qw(time);
@@ -14,7 +14,7 @@ use File::Path qw(make_path);
 
 ## create folder where storing example I/O files
 use File::Path qw(make_path);
-my $basedir= "example/data/";
+my $basedir= "data/";
 make_path($basedir) unless(-d $basedir);
 
 ## note: if case you want to store data in your home, use File::HomeDir
@@ -31,8 +31,21 @@ my $obofile= $basedir."hpo.obo";
 my $hpobo= qx{wget --output-document=$obofile http://purl.obolibrary.org/obo/hp.obo};
 print "HPO obo file downloaded: done\n\n";
 
+## shrink HPO obo file to a subset of terms
+my @terms = qw(HP:0001507 HP:0000008 HP:0002719 HP:0000021 HP:0000023);
+my $termsfile= $basedir."hpoterms.txt";
+open OUT, "> $termsfile";
+foreach my $go (@terms){print OUT "$go\n";}
+close OUT;
+
+$res= obo_filter($obofile, $termsfile);
+my $newobo= $basedir."hpo-shrunk.obo"; 
+open OUT, ">", $newobo; 
+print OUT "${$res}";
+close OUT;
+
 ## extract edges from HPO obo file
-my $hpores= obogaf::parser::build_edges($obofile);
+my $hpores= build_edges($obofile);
 my $hpoedges= $basedir."hpo-edges.txt"; ## hpo edges file declared here 
 open FH, "> $hpoedges"; 
 print FH "${$hpores}"; ## scalar dereferencing
@@ -41,19 +54,19 @@ print "build HPO edges: done\n\n";
 
 ## make stats on HPO 
 ($parentIndex, $childIndex)= (0,1);
-$res= obogaf::parser::make_stat($hpoedges, $parentIndex, $childIndex);
+$res= make_stat($hpoedges, $parentIndex, $childIndex);
 print "$res";
 print "\nHPO stats: done\n\n";
 
 ## compute parents and children list on HPO ontology
 $parlist= $basedir."gobasic-parHPO.txt";
-$pares= obogaf::parser::get_parents_or_children_list($hpoedges, 0,1, "parents");
+$pares= get_parents_or_children_list($hpoedges, 0,1, "parents");
 open FH, "> $parlist";
 foreach my $k (sort{$a cmp $b} keys %$pares) { print FH "$k $$pares{$k}\n";} ## parents  list
 close FH;
 
 $chdlist= $basedir."gobasic-chdHPO.txt";
-$chdres= obogaf::parser::get_parents_or_children_list($hpoedges, 0,1, "children");
+$chdres= get_parents_or_children_list($hpoedges, 0,1, "children");
 open FH, "> $chdlist";
 foreach my $k (sort{$a cmp $b} keys %$chdres) { print FH "$k $$chdres{$k}\n";} ## children list
 close FH;
@@ -67,7 +80,7 @@ my $hpoann= qx{wget --output-document=$hpofile http://compbio.charite.de/jenkins
 
 ## extract HPO annotations 
 ($geneindex, $classindex)= (1,3);
-($res, $stat)= obogaf::parser::gene2biofun($hpofile, $geneindex, $classindex);
+($res, $stat)= gene2biofun($hpofile, $geneindex, $classindex);
 my $hpout= $basedir."hpo.gene2pheno.txt";
 open FH, "> $hpout";
 foreach my $k (sort{$a cmp $b} keys %$res) { print FH "$k $$res{$k}\n";} ## dereferencing
@@ -81,7 +94,7 @@ my $hpofileOld= $basedir."hpo.ann.old.txt"; ## goa annotation file declared here
 my $hpold= qx{wget --output-document=$hpofileOld http://compbio.charite.de/jenkins/job/hpo.annotations.monthly/139/artifact/annotation/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt};
 
 ## map HPO terms between release
-($res, $stat)= obogaf::parser::map_OBOterm_between_release($obofile, $hpofileOld, 3);
+($res, $stat)= map_OBOterm_between_release($obofile, $hpofileOld, 3);
 my $mapfile= $basedir."hpo.ann.mapped.txt";
 open FH, "> $mapfile"; 
 print FH "${$res}";
